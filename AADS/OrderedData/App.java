f7d341216dd074b9007b446111055017e052b17f
PrimalCat-Real <romanenko.denis2003@gmail.com>
2022-11-14 23:20:49 +0200

    Done task #2

--- AADS/OrderedData/App.java
+++ AADS/OrderedData/App.java
@@ -1,16 +1,36 @@
 public class App {
     public static void main(String[] args) {
         // False to disable duplicates
-        ArrayOrdered arrayOrdered = new ArrayOrdered(10, false);
+        ArrayOrdered arrayOrdered = new ArrayOrdered(2, false);
         arrayOrdered.insertValue(2);
         arrayOrdered.insertValue(2);
         arrayOrdered.insertValue(2);
         arrayOrdered.printArray(arrayOrdered.getArray());
+        arrayOrdered.getTwoThirdsOf(arrayOrdered.getArray());
 
         // Task 2
+        // 10
+        ArrayOrdered arrayOrdered_10 = new ArrayOrdered(10, false);
+        arrayOrdered_10.generateArray();
+        arrayOrdered_10.bSearchCounted(arrayOrdered_10.getTwoThirdsOf(arrayOrdered_10.getArray()), arrayOrdered_10.getArray());
+        // 100
         ArrayOrdered arrayOrdered_100 = new ArrayOrdered(100, false);
+        arrayOrdered_100.generateArray();
+        arrayOrdered_100.bSearchCounted(arrayOrdered_100.getTwoThirdsOf(arrayOrdered_100.getArray()), arrayOrdered_100.getArray());
+
+        // 1_000
         ArrayOrdered arrayOrdered_1_000 = new ArrayOrdered(1_000, false);
+        arrayOrdered_1_000.generateArray();
+        arrayOrdered_1_000.bSearchCounted(arrayOrdered_1_000.getTwoThirdsOf(arrayOrdered_1_000.getArray()), arrayOrdered_1_000.getArray());
+
+        // 10_000
         ArrayOrdered arrayOrdered_10_000 = new ArrayOrdered(10_000, false);
+        arrayOrdered_10_000.generateArray();
+        arrayOrdered_10_000.bSearchCounted(arrayOrdered_10_000.getTwoThirdsOf(arrayOrdered_10_000.getArray()), arrayOrdered_10_000.getArray());
+
+        // 100_000
         ArrayOrdered arrayOrdered_100_000 = new ArrayOrdered(100_000, false);
+        arrayOrdered_100_000.generateArray();
+        arrayOrdered_100_000.bSearchCounted(arrayOrdered_100_000.getTwoThirdsOf(arrayOrdered_100_000.getArray()), arrayOrdered_100_000.getArray());
     }
 }
--- AADS/OrderedData/OrderedAbilities.java
+++ AADS/OrderedData/OrderedAbilities.java
@@ -29,7 +29,35 @@ public class OrderedAbilities {
     }
 
     // Task 2
+    public Integer bSearchCounted(int itemToFind, int[] array) {
+        int low = 0;
+        int height = array.length - 1;
+        int guess;
+        int mid;
+        int count = 0;
+        while (low <= height) {
+            mid = (low + height) / 2;
+            guess = array[mid];
+            count +=1;
+            if (guess == itemToFind) {
+                System.out.println("While fires: "+count+" for array length: "+array.length);
+                return mid;
+            } else if (guess > itemToFind) {
+                height = mid - 1;
+            } else if (guess < itemToFind) {
+                low = mid + 1;
+            } else {
+                System.out.println("WTF you guess");
+                break;
+            }
+        }
+        System.out.println("I can't find your item in array: " + itemToFind);
+        return null;
+    }
+
+    // Task 2
     public Integer getTwoThirdsOf(int[] array){
-        return 0;
+        int index = (int) (array.length*0.66);
+        return array[index];
     }
 }
